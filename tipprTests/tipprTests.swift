6dd8939a650dafc18cb7624a45472055084bf151
Raj <rajrana404@gmail.com>
2019-01-07 18:32:54 -0500

    Initial Commit

---
+++ tipprTests/tipprTests.swift
@@ -0,0 +1,34 @@
+//
+//  tipprTests.swift
+//  tipprTests
+//
+//  Created by Raj Rana on 1/7/19.
+//  Copyright © 2019 Raj Rana. All rights reserved.
+//
+
+import XCTest
+@testable import tippr
+
+class tipprTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

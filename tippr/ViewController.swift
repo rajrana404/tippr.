b39e0a4d4f7c065c67dbd853cf7a2d5abaffb20c
Raj <rajrana404@gmail.com>
2019-01-09 16:06:06 -0500

    project commited

--- tippr/ViewController.swift
+++ tippr/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var splitStepper: UIStepper!
+    @IBOutlet weak var eachSplitLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.splitLabel.text = "\(Int(self.splitStepper.value))"
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func splitter(_ sender: UIStepper) {
+        splitLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let split = Double(splitStepper.value)
+        let each = total / split
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        eachSplitLabel.text = String(format: "$%.2f", each)
+        
+    }
+    
+    
 }
 
